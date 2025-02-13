98863e1031f2463557e2d92083d88d2e61867216
aghogho <monorienaghogho@gmail.com>
2025-02-13 22:11:01 +0100

    Remove checkLogin middleware

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -26,7 +26,7 @@ router.post(
 
 router.get(
     "/management", 
-    utilities.checkLogin,
+    // utilities.checkLogin,
     utilities.handleErrors(accountController.buildAccountManagement));
 
 
